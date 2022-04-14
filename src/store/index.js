b6be554f84c34d57677e4695175f1f06922a8d01
Simamkele <simamkelejanuary@gmail.com>
2022-04-14 16:20:55 +0200

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  //this is where we stod our app's data
   state: {
+    counter: 0
   },
+  //getters allow to get data from state
   getters: {
   },
+  // methords that change data in the state ((cant trigure Async code in state))
   mutations: {
+    decreaseCounter(state) {
+      state.counter--
+    },
+    increaseCounter(state, randomNumber) {
+      console.log('randomNumber: ', randomNumber)
+    }
   },
+  //also methords but cannot change data in the state, but can access data in state((can trigure Async code in state))
   actions: {
+    increaseCounter() {
+      console.log('increaseCounter (action)')
+      axiox('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then(response => {
+        this.commit('increaseCounter', response.data)
+            })
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter">{{ $store.state.counter }}</div>
+    <div class="buttons">
+      <button @click="$store.commit('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter')">+</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+<script> 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+name: 'Home',
+// data() {
+//   return {
+//     counter: 0
+//   }
+// },
+
+// methods: {
+//   decreaseCounter() {
+//   this.counter--
+// },
+// increaseCounter() {
+//   this.counter++
+// }
+// }
 }
 </script>
+ 
+ <style >
+ div{
+   margin-bottom: 10px;
+ }
+ .counter{
+   font-size: 80px;
+ }
+ .buttons button {
+   font-size: 40px;
+   width: 100px;
+   margin: 0 10px;
+ }
+ </style>
